abbfda9e5393f300f74e8ac14686351798a76ec5
IsaacAlao <88806554+IsaacAlao@users.noreply.github.com>
2022-10-24 11:55:51 -0400

    Create run_cmds.sh

---
+++ .github/workflows/run_cmds.sh
@@ -0,0 +1,12 @@
+          #!/bin/bash
+          
+          echo What is in this directory? && ls -la
+          echo
+          echo Is Java installed?
+          java --version
+          echo Is Git installed?
+          git --version
+          echo What about build tools?
+          mvn --version
+          gradle --version
+          ant -version
